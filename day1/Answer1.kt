9b67c067e40dbcd29647366e641fabac15c86732
Joshua Sun <joshuasun74@gmail.com>
2019-12-03 01:44:02 +0800

    Update Answer1 for part 2

--- day1/Answer1.kt
+++ day1/Answer1.kt
@@ -7,11 +7,28 @@ fun main(args: Array<String>) {
     File(INPUT_FILE)
         .useLines { it.toList() }
         .forEach { mass ->
-            sum += calculateFuel(mass.toInt())
+            sum += calculateTotalFuel(mass.toInt())
         }
 
     println("Sum of the fuel requirements: $sum")
 }
 
-fun calculateFuel(mass: Int): Int = mass / 3 - 2
+/**
+ * Answer for part2:
+ * Calculates the initial fuel and all of the added fuels for the given [mass].
+ */
+fun calculateTotalFuel(mass: Int): Int {
+    var totalFuel = 0
+    var fuel = calculateFuel(mass)
+    while (fuel > 0) {
+        totalFuel += fuel
+        fuel = calculateFuel(fuel)
+    }
+    return totalFuel
+}
 
+/**
+ * Answer for part1.
+ * Calculates fuel for the given [mass].
+ */
+fun calculateFuel(mass: Int): Int = mass / 3 - 2
